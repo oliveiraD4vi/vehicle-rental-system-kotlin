8fd17ed7c7dffae0d8ee0936fabcb05bcf6fa448
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-05-15 18:35:42 -0300

    Ajustada página de Login e adicionada navegação para cadastro

--- app/src/main/java/com/example/projectmobile/LoginActivity.kt
+++ app/src/main/java/com/example/projectmobile/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.projectmobile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.ImageButton
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var buttonSubmit: Button
+    private lateinit var progressBar: ProgressBar
+    private lateinit var noAccount: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,9 +23,23 @@ class LoginActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val backButton: ImageButton = findViewById(R.id.backButton)
-        backButton.setOnClickListener {
-            finish()
+        editTextEmail = findViewById(R.id.emailEditText)
+        editTextPassword = findViewById(R.id.passwordEditText)
+        buttonSubmit = findViewById(R.id.loginButton)
+        progressBar = findViewById(R.id.progressBar)
+        noAccount = findViewById(R.id.no_account)
+
+        buttonSubmit.setOnClickListener {
+            if (validateFields()) {
+                disableFields()
+                progressBar.visibility = View.VISIBLE
+                sendDataToServer()
+            }
+        }
+
+        noAccount.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 
@@ -25,5 +49,50 @@ class LoginActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun validateFields(): Boolean {
+        val email = editTextEmail.text.toString()
+        val password = editTextPassword.text.toString()
+
+        if (email.isEmpty()) {
+            editTextEmail.error = "Campo obrigatório"
+            return false
+        }
+
+        if (!isValidEmail(email)) {
+            editTextEmail.error = "Email inválido"
+            return false
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.error = "Campo obrigatório"
+            return false
+        }
+
+        return true
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    private fun disableFields() {
+        editTextEmail.isEnabled = false
+        editTextPassword.isEnabled = false
+        buttonSubmit.isEnabled = false
+    }
+
+    private fun enableFields() {
+        editTextEmail.isEnabled = true
+        editTextPassword.isEnabled = true
+        buttonSubmit.isEnabled = true
+    }
+
+    private fun sendDataToServer() {
+        android.os.Handler().postDelayed({
+            progressBar.visibility = View.GONE
+            enableFields()
+        }, 2000)
     }
 }
---
+++ app/src/main/java/com/example/projectmobile/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.example.projectmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
