7b6ea71552025b7dcb9bdc6a64a9720d57e740b2
Robson <robsonad07@gmail.com>
2023-05-30 19:32:30 -0300

    activity_reservations_empty changed layout

--- app/src/main/java/com/example/projectmobile/ui/reservations/ReservationsEmptyActivity.kt
+++ app/src/main/java/com/example/projectmobile/ui/reservations/ReservationsEmptyActivity.kt
@@ -2,10 +2,14 @@ package com.example.projectmobile.ui.reservations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projectmobile.databinding.ActivityMainBinding
 
 class ReservationsEmptyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reservations_empty)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
     }
 }
