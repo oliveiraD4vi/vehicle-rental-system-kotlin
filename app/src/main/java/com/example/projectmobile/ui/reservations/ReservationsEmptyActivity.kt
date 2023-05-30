e4e4641df6110f951bd117cf77245b9a399c9529
Robson <robsonad07@gmail.com>
2023-05-30 19:22:01 -0300

    ActivityReservasEmpty added

---
+++ app/src/main/java/com/example/projectmobile/ui/reservations/ReservationsEmptyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectmobile.ui.reservations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReservationsEmptyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reservations_empty)
+    }
+}
