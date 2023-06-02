51fb291e120553f83750d3c8e64409438c6d6536
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-06-02 12:38:52 -0300

    feat: added create car and create reservation requisitions

--- app/src/main/java/com/example/projectmobile/ui/admin/cars/car/CreateCarActivity.kt
+++ app/src/main/java/com/example/projectmobile/ui/admin/cars/car/CreateCarActivity.kt
@@ -2,21 +2,168 @@ package com.example.projectmobile.ui.admin.cars.car
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.example.projectmobile.api.callback.APICallback
+import com.example.projectmobile.api.service.APIService
+import com.example.projectmobile.api.types.APIResponse
 import com.example.projectmobile.databinding.ActivityCreateCarBinding
+import com.example.projectmobile.util.UserPreferencesManager
+import java.io.IOException
 
 class CreateCarActivity : AppCompatActivity() {
     private var _binding: ActivityCreateCarBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var preferencesManager: UserPreferencesManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityCreateCarBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        preferencesManager = UserPreferencesManager(this)
+
         supportActionBar?.hide()
 
         binding.returnButton.setOnClickListener {
             finish()
         }
+
+        binding.registerButton.setOnClickListener {
+            val brand = binding.editBrand.text.toString()
+            val model = binding.editModel.text.toString()
+            val color = binding.editColor.text.toString()
+            val plate = binding.editPlate.text.toString()
+            val price = binding.editPrice.text.toString()
+
+            if (validateFields(brand, model, color, plate, price)) {
+                sendDataToServer()
+            }
+        }
+    }
+
+    private fun validateFields(
+        brand: String,
+        model: String,
+        color: String,
+        plate: String,
+        price: String,
+    ): Boolean {
+        if (TextUtils.isEmpty(brand)) {
+            binding.editBrand.error = "Campo obrigatório"
+            return false
+        }
+
+        if (TextUtils.isEmpty(model)) {
+            binding.editModel.error = "Campo obrigatório"
+            return false
+        }
+
+        if (TextUtils.isEmpty(color)) {
+            binding.editColor.error = "Campo obrigatório"
+            return false
+        }
+
+        if (TextUtils.isEmpty(plate)) {
+            binding.editPlate.error = "Campo obrigatório"
+            return false
+        }
+
+        if (TextUtils.isEmpty(price)) {
+            binding.editPrice.error = "Campo obrigatório"
+            return false
+        }
+
+        return true
+    }
+
+    private fun sendDataToServer() {
+        loading()
+        val apiService = APIService(preferencesManager.getToken())
+        val url = "/vehicle/register"
+
+        val requestData = getRequestData()
+
+        apiService.postData(url, requestData, object : APICallback {
+            override fun onSuccess(response: APIResponse) {
+                if (!response.error) {
+                    val message = response.message
+
+                    runOnUiThread {
+                        Toast.makeText(
+                            this@CreateCarActivity,
+                            message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    finish()
+                } else {
+                    val errorCode = response.message
+
+                    runOnUiThread {
+                        loaded()
+                        Toast.makeText(
+                            this@CreateCarActivity,
+                            errorCode,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+
+            override fun onError(error: IOException) {
+                runOnUiThread {
+                    loaded()
+                    Toast.makeText(
+                        this@CreateCarActivity,
+                        error.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
+    }
+
+    private fun getRequestData(): String {
+        val brand = binding.editBrand.text.toString()
+        val model = binding.editModel.text.toString()
+        val color = binding.editColor.text.toString()
+        val plate = binding.editPlate.text.toString()
+        val price = binding.editPrice.text.toString()
+
+        return """
+            "brand": "$brand",
+            "model": "$model",
+            "color": "$color",
+            "plate": "$plate",
+            "value": "$price",
+        """.trimIndent()
+    }
+
+    private fun loading() {
+        binding.titleTextView.visibility = View.GONE
+        binding.editBrand.visibility = View.GONE
+        binding.editModel.visibility = View.GONE
+        binding.editColor.visibility = View.GONE
+        binding.editPlate.visibility = View.GONE
+        binding.editPrice.visibility = View.GONE
+        binding.registerButton.visibility = View.GONE
+
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun loaded() {
+        binding.titleTextView.visibility = View.VISIBLE
+        binding.editBrand.visibility = View.VISIBLE
+        binding.editModel.visibility = View.VISIBLE
+        binding.editColor.visibility = View.VISIBLE
+        binding.editPlate.visibility = View.VISIBLE
+        binding.editPrice.visibility = View.VISIBLE
+        binding.registerButton.visibility = View.VISIBLE
+
+        binding.progressBar.visibility = View.GONE
     }
 }
--- app/src/main/java/com/example/projectmobile/ui/admin/reservations/reservation/CreateReservationActivity.kt
+++ app/src/main/java/com/example/projectmobile/ui/admin/reservations/reservation/CreateReservationActivity.kt
@@ -2,21 +2,155 @@ package com.example.projectmobile.ui.admin.reservations.reservation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.example.projectmobile.api.callback.APICallback
+import com.example.projectmobile.api.service.APIService
+import com.example.projectmobile.api.types.APIResponse
 import com.example.projectmobile.databinding.ActivityCreateReservationBinding
+import com.example.projectmobile.util.UserPreferencesManager
+import java.io.IOException
 
 class CreateReservationActivity : AppCompatActivity() {
     private var _binding: ActivityCreateReservationBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var preferencesManager: UserPreferencesManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityCreateReservationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        preferencesManager = UserPreferencesManager(this)
+
         supportActionBar?.hide()
 
         binding.returnButton.setOnClickListener {
             finish()
         }
+
+        binding.registerButton.setOnClickListener {
+            val userId = binding.userId.text.toString()
+            val vehicleId = binding.vehicleId.text.toString()
+            val pickup = binding.pickup.text.toString()
+            val devolution = binding.devolution.text.toString()
+
+            if (validateFields(userId, vehicleId, pickup, devolution)) {
+                sendDataToServer()
+            }
+        }
+    }
+
+    private fun validateFields(
+        userId: String,
+        vehicleId: String,
+        pickup: String,
+        devolution: String,
+    ): Boolean {
+        if (TextUtils.isEmpty(userId)) {
+            binding.userId.error = "Campo obrigatório"
+            return false
+        }
+
+        if (TextUtils.isEmpty(vehicleId)) {
+            binding.vehicleId.error = "Campo obrigatório"
+            return false
+        }
+
+        if (TextUtils.isEmpty(pickup)) {
+            binding.pickup.error = "Campo obrigatório"
+            return false
+        }
+
+        if (TextUtils.isEmpty(devolution)) {
+            binding.devolution.error = "Campo obrigatório"
+            return false
+        }
+
+        return true
+    }
+
+    private fun sendDataToServer() {
+        loading()
+        val apiService = APIService(preferencesManager.getToken())
+        val url = "/reservation/register"
+
+        val requestData = getRequestData()
+
+        apiService.postData(url, requestData, object : APICallback {
+            override fun onSuccess(response: APIResponse) {
+                if (!response.error) {
+                    val message = response.message
+
+                    runOnUiThread {
+                        Toast.makeText(
+                            this@CreateReservationActivity,
+                            message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    finish()
+                } else {
+                    val errorCode = response.message
+
+                    runOnUiThread {
+                        loaded()
+                        Toast.makeText(
+                            this@CreateReservationActivity,
+                            errorCode,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+
+            override fun onError(error: IOException) {
+                runOnUiThread {
+                    loaded()
+                    Toast.makeText(
+                        this@CreateReservationActivity,
+                        error.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
+    }
+
+    private fun getRequestData(): String {
+        val userId = binding.userId.text.toString()
+        val vehicleId = binding.vehicleId.text.toString()
+        val pickup = binding.pickup.text.toString()
+        val devolution = binding.devolution.text.toString()
+
+        return """
+            "userId": "$userId",
+            "vehicleId": "$vehicleId",
+            "pickup": "$pickup",
+            "devolution": "$devolution",
+        """.trimIndent()
+    }
+
+    private fun loading() {
+        binding.titleTextView.visibility = View.GONE
+        binding.idLayout.visibility = View.GONE
+        binding.pickup.visibility = View.GONE
+        binding.devolution.visibility = View.GONE
+        binding.registerButton.visibility = View.GONE
+
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun loaded() {
+        binding.titleTextView.visibility = View.VISIBLE
+        binding.idLayout.visibility = View.VISIBLE
+        binding.pickup.visibility = View.VISIBLE
+        binding.devolution.visibility = View.VISIBLE
+        binding.registerButton.visibility = View.VISIBLE
+
+        binding.progressBar.visibility = View.GONE
     }
 }
