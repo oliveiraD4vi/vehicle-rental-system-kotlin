06fd5b775cf6cda4f1179627888bb9168390aae9
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-06-05 23:38:49 -0300

    fix: changed to older version

--- app/src/main/java/com/example/projectmobile/ui/formreservation/map/MapViewActivity.kt
+++ app/src/main/java/com/example/projectmobile/ui/formreservation/map/MapViewActivity.kt
@@ -1,20 +1,20 @@
 package com.example.projectmobile.ui.formreservation.map
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectmobile.R
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.net.Uri
-import android.os.Bundle
-import android.os.Looper
 import android.widget.Button
 import android.widget.ImageButton
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.projectmobile.MainActivity
-import com.example.projectmobile.R
-import com.google.android.gms.location.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
@@ -24,32 +24,18 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
 
 class MapViewActivity : AppCompatActivity(), OnMapReadyCallback {
-
     private lateinit var mapView: MapView
     private lateinit var googleMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var routePolyline: PolylineOptions
-
-    private val locationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
-            val lastLocation = locationResult.lastLocation
-            if (lastLocation != null) {
-                updateMapWithLocation(lastLocation)
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_view)
-
         supportActionBar?.hide()
-
         val backButton: ImageButton = findViewById(R.id.returnButton)
         backButton.setOnClickListener {
             finish()
         }
-
         val finishButton: Button = findViewById(R.id.finish_button)
         finishButton.setOnClickListener {
             val intent = Intent(this@MapViewActivity, MainActivity::class.java)
@@ -59,21 +45,31 @@ class MapViewActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val viewOnMapButton: Button = findViewById(R.id.view_on_map)
         viewOnMapButton.setOnClickListener {
-            val latitude = AGENCY_LATITUDE
-            val longitude = AGENCY_LONGITUDE
+            val latitude = -4.969732  // Latitude da agência da locadora
+            val longitude = -39.016754  // Longitude da agência da locadora
             val uri = "geo:$latitude,$longitude?q=$latitude,$longitude(Agência da Locadora)"
             val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
-            mapIntent.setPackage("com.google.android.apps.maps")
+            mapIntent.setPackage("com.google.android.apps.maps")  // Define o pacote do aplicativo de mapas do Google
             startActivity(mapIntent)
         }
 
         mapView = findViewById(R.id.mapView)
         mapView.onCreate(savedInstanceState)
 
-        if (checkLocationPermissions()) {
+        // Verificar permissões de localização
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             initializeMap()
         } else {
-            requestLocationPermissions()
+            // Solicitar permissão de localização, se não estiver concedida
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
         }
     }
 
@@ -95,13 +91,11 @@ class MapViewActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onPause() {
         super.onPause()
         mapView.onPause()
-        stopLocationUpdates()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         mapView.onDestroy()
-        stopLocationUpdates()
     }
 
     override fun onLowMemory() {
@@ -112,30 +106,33 @@ class MapViewActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
 
-        if (checkLocationPermissions()) {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Obter a localização atual do usuário
             fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
                 location?.let {
-                    setupMap(location)
-                }
-            }
-        }
-
-        val updateLocationButton: ImageButton = findViewById(R.id.update_location_button)
-        updateLocationButton.setOnClickListener {
-            if (checkLocationPermissions()) {
-                if (ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    fusedLocationClient.requestLocationUpdates(
-                        LocationRequest.create(),
-                        locationCallback,
-                        Looper.getMainLooper()
+                    // Personalize o mapa conforme suas necessidades, como definir a posição inicial e adicionar um marcador
+                    val latitude = -4.969732  // Latitude da agência da locadora
+                    val longitude = -39.016754  // Longitude da agência da locadora
+                    val agencyLocation = LatLng(latitude, longitude)
+                    googleMap.addMarker(
+                        MarkerOptions().position(agencyLocation).title("Agência da Locadora")
                     )
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(agencyLocation, 16f))
+                    // Configurar a rota com dois pontos: localização atual do usuário e agência da locadora
+                    val userLocation = LatLng(location.latitude, location.longitude)
+                    val routePolylineOptions = PolylineOptions()
+                        .add(userLocation)
+                        .add(agencyLocation)
+                        .width(5f)
+                        .color(ContextCompat.getColor(this, R.color.route_color))
+                    googleMap.addPolyline(routePolylineOptions)
                 }
             }
         }
@@ -148,33 +145,10 @@ class MapViewActivity : AppCompatActivity(), OnMapReadyCallback {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (checkLocationPermissions()) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 initializeMap()
             }
         }
-    }
-
-    private fun checkLocationPermissions(): Boolean {
-        val fineLocationPermission = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-        val coarseLocationPermission = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-        return fineLocationPermission && coarseLocationPermission
-    }
-
-    private fun requestLocationPermissions() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ),
-            LOCATION_PERMISSION_REQUEST_CODE
-        )
     }
 
     private fun initializeMap() {
@@ -182,34 +156,7 @@ class MapViewActivity : AppCompatActivity(), OnMapReadyCallback {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
-    private fun stopLocationUpdates() {
-        fusedLocationClient.removeLocationUpdates(locationCallback)
-    }
-
-    private fun setupMap(location: Location) {
-        val agencyLocation = LatLng(AGENCY_LATITUDE, AGENCY_LONGITUDE)
-        googleMap.addMarker(
-            MarkerOptions().position(agencyLocation).title("Agência da Locadora")
-        )
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(agencyLocation, 16f))
-
-        val userLocation = LatLng(location.latitude, location.longitude)
-        routePolyline = PolylineOptions()
-            .add(userLocation)
-            .add(agencyLocation)
-            .width(5f)
-            .color(ContextCompat.getColor(this, R.color.route_color))
-        googleMap.addPolyline(routePolyline)
-    }
-
-    private fun updateMapWithLocation(location: Location) {
-        googleMap.clear()
-        setupMap(location)
-    }
-
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 123
-        private const val AGENCY_LATITUDE = -4.969732
-        private const val AGENCY_LONGITUDE = -39.016754
     }
 }
